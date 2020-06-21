fb7f93ccc61c20dd068c7be853d0dce894456b7d
braimahabiola <braimahabiola.official@gmail.com>
2020-06-21 15:46:44 +0000

    Added function for counter

--- js/theme.js
+++ js/theme.js
@@ -83,6 +83,31 @@
       type: 'image'
   });
 
+// Counter
+
+const counters = document.querySelectorAll('.counter');
+const speed = 200;
+
+counters.forEach(counter => {
+  const updateCount = () => {
+    const target = +counter.getAttribute('data-target');
+    const count = +counter.innerText;
+
+    const inc = target / speed;
+
+    if(count < target){
+      counter.innerText = Math.ceil(count + inc);
+      setTimeout(updateCount, 1);
+    } else{
+      count.innerText = target;
+    }
+  }
+
+  updateCount();
+});
+
+// End Counter
+
   /*----------------------------------------------------*/
   /*  Isotope Fillter js
     /*----------------------------------------------------*/
